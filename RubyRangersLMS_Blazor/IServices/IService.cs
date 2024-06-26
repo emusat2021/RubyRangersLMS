c11f904ede9d8814179408887f921f1c9900365a
DESKTOP-B8DCFDH\mohan <mohanad-yousif@outlook.com>
2024-06-26 16:45:01 +0200

    add HttpClient configration with Student configration

---
+++ RubyRangersLMS_Blazor/IServices/IService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace RubyRangersLMS_Blazor.IServices
+{
+    public interface IService<T>
+    {
+
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+    }
+}
---
+++ RubyRangersLMS_Blazor/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace RubyRangersLMS_Blazor.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public Guid CourseId { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+    }
+}
