522e6c36a4a4a6dfa72d1772854f4a5b76857e29
Eugenia <musat.eugenia@gmail.com>
2024-06-27 09:24:51 +0200

    Create Teacher Service

---
+++ RubyRangersLMS_Blazor/Services/TeacherServices.cs
@@ -0,0 +1,31 @@
+﻿using RubyRangersLMS_Blazor.IServices;
+using RubyRangersLMS_Blazor.Models;
+
+namespace RubyRangersLMS_Blazor.Services
+{
+    public class TeacherServices : ITeacherServices
+    {
+        private readonly HttpClient _httpClient;
+        public TeacherServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> CreateTeachersAsync(TeacherCreateModel teacherCreateModel)
+        {
+            var response = await _httpClient.PostAsJsonAsync("teachers", teacherCreateModel);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteTeachersAsync(Guid id)
+        {
+            var response = await _httpClient.DeleteAsync($"teachers/{id}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<IEnumerable<TeacherViewModel>> GetTeachersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<TeacherViewModel>>("teachers");
+        }
+    }
+}
