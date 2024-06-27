3364c0f1eafbc3464a8ffb4280243dda831f2bfb
Eugenia <musat.eugenia@gmail.com>
2024-06-27 09:22:15 +0200

    Create Teacher model och view model

---
+++ RubyRangersLMS_Blazor/Models/TeacherCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RubyRangersLMS_Blazor.Models
+{
+    public class TeacherCreateModel
+    {
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ RubyRangersLMS_Blazor/Models/TeacherViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RubyRangersLMS_Blazor.Models
+{
+    public class TeacherViewModel
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
