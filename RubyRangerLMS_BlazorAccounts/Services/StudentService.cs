f00c288245f084856655a1295d52d7689dfd0cbf
Victor S <victor.skarerverket@gmail.com>
2024-06-28 11:36:36 +0200

    Added Models and Services from Yousifs code struct

---
+++ RubyRangerLMS_BlazorAccounts/Services/StudentService.cs
@@ -0,0 +1,39 @@
+﻿using RubyRangerLMS_BlazorAccounts.Models;
+
+namespace RubyRangerLMS_BlazorAccounts.Services
+{
+    public class StudentService : IService<Student>
+    {
+        private readonly HttpClient httpClient;
+
+        public StudentService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            var response = await httpClient.GetAsync("api/student");
+            return await response.Content.ReadFromJsonAsync<List<Student>>() ?? new List<Student>();
+        }
+
+        public async Task<Student> GetByIdAsync(Guid id)
+        {
+            var response = await httpClient.GetAsync($"api/student/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Student>() ?? new Student();
+        }
+        public async Task<Student> UpdateAsync(Guid id, Student student)
+        {
+            var response = await httpClient.PostAsJsonAsync($"api/student/update/{id}", student);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Student>() ?? new Student();
+        }
+
+        public async void DeleteAsync(Guid id)
+        {
+            var response = await httpClient.DeleteAsync($"api/student/delete/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
