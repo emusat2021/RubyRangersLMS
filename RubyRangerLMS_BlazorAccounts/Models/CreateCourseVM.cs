cb86271f896a4b992daaca7fae6b9b2207903c7d
Victor S <victor.skarerverket@gmail.com>
2024-06-29 19:42:19 +0200

    Added functionality to send Course to API
    I'm deliberately not using IService in order to save time.
    Rather have a function working and add Tech debt than not have it at all.

---
+++ RubyRangerLMS_BlazorAccounts/Models/CreateCourseVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RubyRangerLMS_BlazorAccounts.Models
+{
+    public class CreateCourseVM
+    {
+        [Required]
+        public Guid TeacherId { get; set; }
+
+        [Required]
+        [Display(Name = "Course Name")]
+        [DataType(DataType.Text)]
+        [StringLength(32, ErrorMessage = "Course Name needs to be between 2 - 32 Characters.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Display(Name = "Course Description")]
+        [DataType(DataType.MultilineText)]
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ RubyRangerLMS_BlazorAccounts/Services/CourseService.cs
@@ -0,0 +1,21 @@
+﻿using RubyRangerLMS_BlazorAccounts.Models;
+
+namespace RubyRangerLMS_BlazorAccounts.Services
+{
+    public class CourseService
+    {
+        private readonly HttpClient httpClient;
+        // Remember the url to the api sometimes changes.
+
+        public CourseService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<bool> CreateCourseAsync(CreateCourseVM model)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/update/", model);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
