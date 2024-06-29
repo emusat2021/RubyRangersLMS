b563d7debfa0120cfe960dc15b2bad15ef187b41
Victor S <victor.skarerverket@gmail.com>
2024-06-29 21:51:33 +0200

    Tried updating CourseService to send api request
    It doesn't work properly but that's probably a backend issue.

--- RubyRangerLMS_BlazorAccounts/Services/CourseService.cs
+++ RubyRangerLMS_BlazorAccounts/Services/CourseService.cs
@@ -6,10 +6,11 @@ namespace RubyRangerLMS_BlazorAccounts.Services
     {
         private readonly HttpClient httpClient;
         // Remember the url to the api sometimes changes.
-
+        // Needs Fixing!
         public CourseService(HttpClient httpClient)
         {
             this.httpClient = httpClient;
+            httpClient.BaseAddress = new Uri("https://localhost:53192/");
         }
 
         public async Task<bool> CreateCourseAsync(CreateCourseVM model)
