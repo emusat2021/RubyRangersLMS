333418b2e62945eb2d9015a7546cd09040fef9d1
Victor S <victor.skarerverket@gmail.com>
2024-07-01 17:00:49 +0200

    Added UseAuthorization

--- RubyRangerLMS_BlazorAccounts/Program.cs
+++ RubyRangerLMS_BlazorAccounts/Program.cs
@@ -62,7 +62,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseAuthorization();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
