38a395722666545ce71197c3e3da5ef17173227e
Eugenia <musat.eugenia@gmail.com>
2024-07-01 12:37:24 +0200

    Add Teacher Model

---
+++ RubyRangerLMS_BlazorAccounts/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace RubyRangerLMS_BlazorAccounts.Models
+{
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
